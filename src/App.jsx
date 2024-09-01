cccb1a0bf8d73638755b50b57c8995e85b770db0
iamvipora <henrydizon99@gmail.com>
2024-09-02 00:30:47 +0800

    add ScrollToTop feature/component for when changing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Index from './pages/Index'
 import Pricing from './pages/Pricing'
+import ScrollToTop from './components/ScrollToTop'
 import './App.css'
 
 function App() {
   return (
     // Website routing
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route index element={<Index />}/>
         <Route path='/pricing' element={<Pricing />}/>
